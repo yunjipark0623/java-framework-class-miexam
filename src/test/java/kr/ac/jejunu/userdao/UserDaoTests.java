87cfee43c4d6684ad44753db3abab4aeb24440cb
박윤지 <bearji0623@gmail.com>
2019-04-19 16:35:40 +0900

    exam 5

--- src/test/java/kr/ac/jejunu/userdao/UserDaoTests.java
+++ src/test/java/kr/ac/jejunu/userdao/UserDaoTests.java
@@ -1,6 +1,9 @@
 package kr.ac.jejunu.userdao;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.sql.SQLException;
 
@@ -8,13 +11,19 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
 public class UserDaoTests {
+    private UserDao userDao;
+
+    @Before
+    public void setup() {
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(DaoFactory.class);
+        userDao = applicationContext.getBean("userDao", UserDao.class);
+    }
+
     @Test
     public void testGet() throws SQLException, ClassNotFoundException {
         Long id = 1l;
         String name = "허윤호";
         String password = "1234";
-        DaoFactory daoFactory = new DaoFactory();
-        UserDao userDao = daoFactory.getUserDao();
         User user = userDao.get(id);
         assertThat(user.getId(), is(id));
         assertThat(user.getName(), is(name));
@@ -24,8 +33,6 @@ public class UserDaoTests {
     @Test
     public void testAdd() throws SQLException, ClassNotFoundException {
         User user = new User();
-        DaoFactory daoFactory = new DaoFactory();
-        UserDao userDao = daoFactory.getUserDao();
 
         String name = "헐크";
         String password = "1111";
