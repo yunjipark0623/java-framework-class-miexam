f0ec319bddedc9a3617a96e4e36834e0747a87da
박윤지 <bearji0623@gmail.com>
2019-04-19 16:27:49 +0900

    exam 4

--- src/test/java/kr/ac/jejunu/userdao/UserDaoTests.java
+++ src/test/java/kr/ac/jejunu/userdao/UserDaoTests.java
@@ -13,7 +13,8 @@ public class UserDaoTests {
         Long id = 1l;
         String name = "허윤호";
         String password = "1234";
-        UserDao userDao = new UserDao(new JejuConnectionMaker());
+        DaoFactory daoFactory = new DaoFactory();
+        UserDao userDao = daoFactory.getUserDao();
         User user = userDao.get(id);
         assertThat(user.getId(), is(id));
         assertThat(user.getName(), is(name));
@@ -23,7 +24,8 @@ public class UserDaoTests {
     @Test
     public void testAdd() throws SQLException, ClassNotFoundException {
         User user = new User();
-        UserDao userDao = new UserDao(new JejuConnectionMaker());
+        DaoFactory daoFactory = new DaoFactory();
+        UserDao userDao = daoFactory.getUserDao();
 
         String name = "헐크";
         String password = "1111";
@@ -38,16 +40,4 @@ public class UserDaoTests {
         assertThat(resultUser.getName(), is(name));
         assertThat(resultUser.getPassword(), is(password));
     }
-
-//    @Test
-//    public void testHallaGet() throws SQLException, ClassNotFoundException {
-//        Long id = 1l;
-//        String name = "헐크";
-//        String password = "1111";
-//        UserDao userDao = new UserDao(new HallaConnectionMaker());
-//        User user = userDao.get(id);
-//        assertThat(user.getId(), is(id));
-//        assertThat(user.getName(), is(name));
-//        assertThat(user.getPassword(), is(password));
-//    }
 }
